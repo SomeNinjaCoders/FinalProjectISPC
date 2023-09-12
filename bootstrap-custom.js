6dbc0c6fe60908f9f9a486bcf1e5274a43db0869
MelisaSG <kianetomoe@gmail.com>
2023-09-12 01:35:20 -0300

    se agregan algunos estilos se posiciona navegador

--- bootstrap-custom.js
+++ bootstrap-custom.js
@@ -33,3 +33,18 @@ if (window.matchMedia("(min-width: 768px)").matches) {
 } else {
 	$(multipleCardCarousel).addClass("slide");
 }
+
+function menuDesplegable(){
+    const nav = document.querySelector("#nav")
+    const abrir = document.querySelector("#abrir")
+    const cerrar = document.querySelector("#cerrar")
+
+    abrir.addEventListener("click", () => {
+        nav.classList.add("visible")
+    })
+
+    cerrar.addEventListener("click", () => {
+        nav.classList.remove("visible")
+})
+}
+
