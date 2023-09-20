7604a36e7302cb1df69b6aa517bca3027bf59107
Edgar F Herrera <132973538+EdgarHerrera2023@users.noreply.github.com>
2023-09-20 19:23:44 -0300

    Add files via upload

---
+++ producto.py
@@ -0,0 +1,8 @@
+class Producto:
+    def __init__(self, idProducto, precio, descripcion):
+        self.idProducto = idProducto
+        self.precio = precio
+        self.descripcion = descripcion
+
+    def mostrar(self):
+        # Lógica para mostrar información del producto
---
+++ usuario.py
@@ -0,0 +1,19 @@
+class Usuario:
+    def __init__(self, idUsuario, nombre, direccion, email, contraseña):
+        self.idUsuario = idUsuario
+        self.nombre = nombre
+        self.direccion = direccion
+        self.email = email
+        self.contraseña = contraseña
+
+    def ingreso(self):
+        # Lógica para el ingreso del usuario
+
+    def consulta(self):
+        # Lógica para consultar información del usuario
+
+    def modificarUsuario(self):
+        # Lógica para modificar información del usuario
+
+    def eliminar(self):
+        # Lógica para eliminar el usuario
